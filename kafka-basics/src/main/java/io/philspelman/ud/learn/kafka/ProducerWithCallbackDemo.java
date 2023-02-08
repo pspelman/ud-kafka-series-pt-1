47dcc841b05ae4195c5cbc8f805ebd51ced68f08
Philip Spelman <pspelman@autofi.io>
2023-02-08 11:30:38 -0600

    - added ProducerWithCallbackDemo - added metadata to producer log

--- kafka-basics/src/main/java/io/philspelman/ud/learn/kafka/ProducerWithCallbackDemo.java
+++ kafka-basics/src/main/java/io/philspelman/ud/learn/kafka/ProducerWithCallbackDemo.java
@@ -1,17 +1,19 @@
 package io.philspelman.ud.learn.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemo {
+public class ProducerWithCallbackDemo {
 
     //    add a logger to the producer
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    private static final Logger log = LoggerFactory.getLogger(ProducerWithCallbackDemo.class.getSimpleName());
 
     public static void main(String[] args) {
 
@@ -38,11 +40,29 @@ public class ProducerDemo {
         //        create Producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
+
+
         //        create a Producer Record
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world with callback");
 
         //        send (produce) data
-        producer.send(producerRecord);
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception e) {
+                // executes every time a record is successfully sent or an exception is thrown
+                if (e == null) {
+                    // record was successfully sent
+                    log.info("Received new metadata\n" +
+                            "Topic: " + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition() + "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+                } else {
+                    // an error occurred while producing
+                    log.error("Error while producing: ", e);
+                }
+            }
+        });
 
         //        tell producer to send all data and block until done -- synchronous
         producer.flush();
