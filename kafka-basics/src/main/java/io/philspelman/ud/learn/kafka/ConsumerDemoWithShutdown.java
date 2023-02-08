d3e682d22ea265fcbdd9ebc02f4c56921dd0198d
Philip Spelman <pspelman@autofi.io>
2023-02-08 17:40:56 -0600

    - finished demo of ConsumerDemoWithShutdown

--- kafka-basics/src/main/java/io/philspelman/ud/learn/kafka/ConsumerDemoWithShutdown.java
+++ kafka-basics/src/main/java/io/philspelman/ud/learn/kafka/ConsumerDemoWithShutdown.java
@@ -3,6 +3,7 @@ package io.philspelman.ud.learn.kafka;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,20 +47,48 @@ public class ConsumerDemoWithShutdown {
         // create a consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
-        // subscribe to a topic
-        consumer.subscribe(Arrays.asList(topic));
+        // before subscribing, create a shutdown hook
+        // get a reference to the main thread
+        final Thread mainThread = Thread.currentThread();
 
-        // begin to poll for data
-        while (true) {
-            log.info("Polling...");
-            // set a timeout for time between polling so as not to overload Kafka broker
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+        // adding a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run(){
+                log.info("Detected a shutdown, exiting with consumer.wakeup() ...");
+                consumer.wakeup(); // this means an error will be thrown the next time the consumer wakes up
 
-            // consume / handle any available data
-            for (ConsumerRecord<String, String> record: records) {
-                log.info("Key: " + record.key() + " , Value: " + record.value());
-                log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                // join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        });
+
+        try {
+            // subscribe to a topic
+            consumer.subscribe(Arrays.asList(topic));
+
+            // poll for data
+            while (true) {
+                log.info("Polling...");
+                // set a timeout for time between polling so as not to overload Kafka broker
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+                // consume / handle any available data
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: " + record.key() + " , Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Consumer is starting to shutdown...");
+        } catch (Exception e) {
+            log.error("Unexpected exception in the consumer", e);
+        } finally {
+            consumer.close(); // close the consumer, this will also commit offsets
+            log.info("Consumer is now gracefully shut down");
         }
 
     }
